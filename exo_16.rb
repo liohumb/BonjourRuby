264fe6560fbe3ab50c2d09f4b5c7d959c18f44b1
Lionel Humbert <helloliohumb@gmail.com>
2021-04-13 16:53:53 +0200

    La pyramide, dans l'autre sens

---
+++ exo_16.rb
@@ -0,0 +1,23 @@
+#pyramide.rb
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+puts "Choisi un nombre entre 1 et 25"
+print ">"
+nombre = gets.chomp.to_i
+x = 1
+y = nombre -1
+
+if(nombre >= 1) & (nombre <= 25)
+  puts "Voici la pyramide :"
+    nombre.times do
+      y.times do
+        print " "
+      end
+      y -= 1
+      x.times do
+        print "#"
+      end
+      x += 1
+      puts " "
+    end
+  else puts "Ceci n'est pas une pyramide car ce n'est pas un numéro entre 1 et 25"
+end
