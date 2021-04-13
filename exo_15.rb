c54f134580dcc24eea50364a581758feabc5779c
Lionel Humbert <helloliohumb@gmail.com>
2021-04-13 15:37:28 +0200

    La pyramyde

--- exo_15.rb
+++ exo_15.rb
@@ -1,14 +1,19 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+puts "Choisi un nombre entre 1 et 25"
 print ">"
 nombre = gets.chomp.to_i
 x = 1
 y = nombre -1
-
-nombre.times do
-  y -= 1
-  x.times do
-    print "#"
-  end
-  x += 1
-  puts " "
+ puts "Voici la pyramide :"
+if(nombre >= 1) & (nombre <= 25)
+  then
+    nombre.times do
+      y -= 1
+      x.times do
+      print "#"
+    end
+      x += 1
+      puts " "
+    end
+  else puts "Ceci n'est pas une pyramide car ce n'est pas un numéro entre 1 et 25"
 end
