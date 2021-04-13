9270710424de0774f6c304624ea864f69f960c2c
Lionel Humbert <helloliohumb@gmail.com>
2021-04-13 12:13:40 +0200

    Afficher lees années

--- exo_09.rb
+++ exo_09.rb
@@ -1,4 +1,6 @@
 puts "En quelle année es tu née?"
-date = gets.chomp
-
-date.to_i.upto(2021) { |i| puts "#{i}"}
+date1 = gets.chomp.to_i
+date2 = 2021 - date1 + 1
+date2.times do |i|
+  puts "#{date1 + i}"
+end
